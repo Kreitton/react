edc7c5282afbfa8bf945db81469e03f0ac55d434
HooplaCode <vkreitton@gmail.com>
2023-07-07 19:40:17 -0400

    component added

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FiMenu} from 'react-icons/fi'
+
+export const Navbar = () => {
+  return (
+    <div > 
+        <FiMenu color='black'/>
+    </div>
+  )
+}
---
+++ components/Seachbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Seachbar = () => {
+  return (
+    <div >
+        
+    </div>
+  )
+}
+
+export default Seachbar
